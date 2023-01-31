f5bb026ed10d585c85e79914f1ae95126691d777
Казанцев Степан <kazantcev.stepan@gmail.com>
2023-01-31 22:57:01 +0500

    task i partially fixed

--- done/i.py
+++ done/i.py
@@ -1,7 +1,13 @@
 # Готовые задачи в попке done
-mock_processors = [3, 2, 6, 4]
-mock_tasks = [[1, 3], [2, 5], [3, 7], [4, 10], [5, 5], [6, 100], [9, 2], ]
-correct_result = 105
+# mock_processors = [3, 2, 6, 4]
+# mock_tasks = [[1, 3], [2, 5], [3, 7], [4, 10], [5, 5], [6, 100], [9, 2], ]
+# correct_result = 105
+
+
+# mock_processors_count, mock_task_count = [1, 1]
+mock_processors = [1000000]
+mock_tasks = [ [1000000000, 1000000] ]
+correct_result = 1000000000000
 
 
 # input
@@ -19,38 +25,29 @@ correct_result = 105
 # 105
 
 def calculate_tasks_completing_time(tasks, processors):
-    work_time = tasks[:-2:-1][0][0] # время запуска последней задачи
 
     processes_loading = {proc: 0 for proc in sorted(processors)}
-    tasks_dict = {time: duration for time, duration in tasks}
     total_energy = 0
+    prev_task = ()
     # можно делать не на каждую секнду, а на кажды процесс
 
-    # Распределяем задачи по процессам каждый тик
-    for sec in range(1, work_time + 1):
+    for time_appearance, time_duration in tasks:
+
+        task_is_distributed = False
 
         for pr_num, pr_load in processes_loading.items():
-            pr_lost_time = processes_loading[pr_num]
-            if pr_lost_time == 0:
-                processes_loading[pr_num] = pr_lost_time
-            else:
-                total_energy += pr_num
-                processes_loading[pr_num] = pr_lost_time - 1
 
-        current_task = tasks_dict.get(sec, None)
-        if not current_task:
-            continue
+            if pr_load:
+                process_time_left = pr_load - (time_appearance - prev_task[0])
+                processes_loading[pr_num] = process_time_left if process_time_left > 0 else 0
 
-        free_process = {proc: lost_time for proc, lost_time in processes_loading.items() if lost_time == 0}
+            if not processes_loading[pr_num] and not task_is_distributed:
+                processes_loading[pr_num] = time_duration
+                total_energy += time_duration * pr_num
+                task_is_distributed = True
 
-        if not free_process:
-            continue
 
-        fast_process = min(free_process)
-        processes_loading[fast_process] = current_task
-
-    # Прибавим оствшиеся на процессорах задачи
-    total_energy += sum(lost_time * proc for proc, lost_time in processes_loading.items())
+        prev_task = (time_appearance, time_duration)
 
     return total_energy
 
