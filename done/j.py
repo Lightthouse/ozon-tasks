c133e4368ed72cf49f0154a4143447bf5dcff8e8
Казанцев Степан <kazantcev.stepan@gmail.com>
2023-01-31 16:10:33 +0500

    partly fixed j task

--- done/j.py
+++ done/j.py
@@ -1,10 +1,9 @@
-# input_dictionary = ['id']
+# mock_dictionary = ['mask', 'blonde', 'maiden']
 mock_dictionary = ['task', 'decide', 'id']
 
 mock_request = ['flask', 'code', 'void', 'forces', 'id', 'ask']
 
 correct_results = ['task', 'decide', 'id', 'task', 'decide', 'task']
-
 
 # input
 # 3
@@ -27,52 +26,55 @@ correct_results = ['task', 'decide', 'id', 'task', 'decide', 'task']
 # decide
 # task
 
+# можно сделать три массива с обрезанным словарем и провеять, есть ли кусок слова в этих словарях
+
 MAX_RHYME_LENGTH = 3
-def create_reverse_dict(dictionary: [str]):
-    dict_with_reverse_words = [i[::-1] for i in dictionary]
-    reverse_dict = {}
-    for current_word in dict_with_reverse_words:
-        max_word_offset = MAX_RHYME_LENGTH if len(current_word) >= MAX_RHYME_LENGTH else len(current_word)
-
-        for word_offset in range(max_word_offset, 0, -1):
-            if reverse_dict.get(current_word[:word_offset], None):
-                reverse_dict[current_word[:word_offset]].append(current_word[::-1])
-            else:
-                reverse_dict[current_word[:word_offset]] = [current_word[::-1]]
-
-    return reverse_dict
 
 
-def find_word_rhyme(word: str, dictionary: [str], reverse_dictionary):
-    def make_rhyme(word_for_rhyme, dict_list, reverse_dict):
-        rhyme = dict_list[0]
-        reverse_word = word_for_rhyme[::-1]
+def create_rhyme_dictionary(dictionary: [str]):
+    rhymes_dictionary = {}
+    for current_word in dictionary:
 
-        for word_offset in range(MAX_RHYME_LENGTH, 0, -1):
-            rhymes_list = reverse_dict.get(reverse_word[:word_offset], None)
-            if rhymes_list:
-                rhyme = rhymes_list[0]
+        for offset in range(1, 4):
 
-            if rhyme == word_for_rhyme:
-                try:
-                    rhyme = rhymes_list[1]
-                    break
-                except IndexError:
-                    pass
+            rhyme_part = current_word[-offset:]
+            if len(rhyme_part) < offset:
+                continue
 
-        rhyme = rhyme if not word == rhyme else dictionary[1]
-        return rhyme
+            if rhymes_dictionary.get(rhyme_part):
+                rhymes_dictionary[rhyme_part].append(current_word)
+                continue
+            rhymes_dictionary[rhyme_part] = [current_word]
 
-    rhyme = make_rhyme(word, dictionary, reverse_dictionary)
+    return rhymes_dictionary
+
+
+def make_rhyme(word: str, dictionary: [str], rhyme_dictionary):
+    rhyme = dictionary[1] if dictionary[0] == word else dictionary[0]
+
+    for word_offset in range(MAX_RHYME_LENGTH, 0, -1):
+        rhyme_key = word[-word_offset:]
+        if not rhyme_dictionary.get(rhyme_key):
+            continue
+
+        if not rhyme_dictionary[rhyme_key][0] == word:
+            return rhyme_dictionary[rhyme_key][0]
+
+        try:
+            return rhyme_dictionary[rhyme_key][1]
+        except:
+            pass
+
+
 
     return rhyme
 
 
 def find_rhymes(words: [str], dictionary):
     res = []
-    reverse_dict = create_reverse_dict(dictionary)
+    rhyme_dictionary = create_rhyme_dictionary(dictionary)
     for word in words:
-        res.append(find_word_rhyme(word, dictionary, reverse_dict))
+        res.append(make_rhyme(word, dictionary, rhyme_dictionary))
     return res
 
 
@@ -91,5 +93,4 @@ for r in find_rhymes(words, dictionary):
 
 
 # local test variant
-# print(*find_rhymes(mock_request, mock_dictionary))
-
+# print(find_rhymes(mock_request, mock_dictionary))
